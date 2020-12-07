e235601b58ce761243cb718b7f77b7f2607b7836
Pulkit <khuranapulkit.pk@gmail.com>
2020-12-07 18:29:26 +0530

    Added all techniques

--- carving/algo.py
+++ carving/algo.py
@@ -46,10 +46,8 @@ def rotate_image(image, clockwise):
     return np.rot90(image, k)
 
 
+@jit
 def backward_energy(im):
-    """
-    Simple gradient magnitude energy map.
-    """
 
     xgrad = ndi.convolve1d(im, np.array([1, 0, -1]), axis=1, mode='wrap')
     ygrad = ndi.convolve1d(im, np.array([1, 0, -1]), axis=0, mode='wrap')
@@ -59,11 +57,50 @@ def backward_energy(im):
     return grad_mag
 
 
+
+@jit
+def forward_energy(im):
+    """
+    Forward energy algorithm as described in "Improved Seam Carving for Video Retargeting"
+    by Rubinstein, Shamir, Avidan.
+
+    """
+    h, w = im.shape[:2]
+    im = cv2.cvtColor(im.astype(np.uint8), cv2.COLOR_BGR2GRAY).astype(np.float64)
+
+    energy = np.zeros((h, w))
+    m = np.zeros((h, w))
+
+    U = np.roll(im, 1, axis=0)
+    L = np.roll(im, 1, axis=1)
+    R = np.roll(im, -1, axis=1)
+
+    cU = np.abs(R - L)
+    cL = np.abs(U - L) + cU
+    cR = np.abs(U - R) + cU
+
+    for i in range(1, h):
+        mU = m[i - 1]
+        mL = np.roll(mU, 1)
+        mR = np.roll(mU, -1)
+
+        mULR = np.array([mU, mL, mR])
+        cULR = np.array([cU[i], cL[i], cR[i]])
+        mULR += cULR
+
+        argmins = np.argmin(mULR, axis=0)
+        m[i] = np.choose(argmins, mULR)
+        energy[i] = np.choose(argmins, cULR)
+
+    return energy
+
+
 @jit
 def remove_seam(im, boolmask):
     h, w = im.shape[:2]
     boolmask3c = np.stack([boolmask] * 3, axis=2)
     return im[boolmask3c].reshape((h, w - 1, 3))
+
 
 
 @jit
@@ -88,16 +125,17 @@ def add_seam(im, seam_idx):
 
     return output
 
+
 @jit
-def get_minimum_seam(im):
+def get_minimum_seam(im, fn = "Backward"):
     h, w = im.shape[:2]
 
-    energyfn = backward_energy
+    if fn == "Backward":
+        energyfn = backward_energy
+    elif fn == "Forward":
+        energyfn = forward_energy
 
     M = energyfn(im)
-
-    # print(M.shape)
-
 
     backtrack = np.zeros_like(M, dtype=np.int)
 
@@ -128,9 +166,9 @@ def get_minimum_seam(im):
     return np.array(seam_idx), boolmask
 
 
-def seams_removal(im, num_remove, vis=False, rot=False):
+def seams_removal(im, num_remove, vis=False, rot=False, fn = "Backward"):
     for _ in range(num_remove):
-        seam_idx, boolmask = get_minimum_seam(im)
+        seam_idx, boolmask = get_minimum_seam(im, fn=fn)
         if vis:
             visualize(im, boolmask, rotate=rot)
         im = remove_seam(im, boolmask)
@@ -165,33 +203,244 @@ def seam_insertion(im, num_add, vis = False, rot = False):
     return im
 
 
-def seam_carve(im, dy, dx, vis=False):
+def seam_carve(im, dy, dx, vis=False, fn="Backward"):
     im = im.astype(np.float64)
     h, w = im.shape[:2]
     # print(h,w)
 
+    if fn == "Backward":
+        M = backward_energy(im)
+    elif fn == "Forward":
+        M = forward_energy(im)
+
+    content = np.sum(M)
+
     output = im
 
     if dx < 0:
-        output = seams_removal(output, -dx, vis)
-
-    elif dx > 0:
-        output = seam_insertion(output, dx, vis)
+        output = seams_removal(output, -dx, vis, fn=fn)
 
     if dy < 0:
         output = rotate_image(output, True)
-        output = seams_removal(output, -dy, vis, rot=True)
+        output = seams_removal(output, -dy, vis, rot=True, fn=fn)
         output = rotate_image(output, False)
 
-    elif dy > 0:
-        output = rotate_image(output, True)
-        output = seam_insertion(output, dy, vis, rot=True)
-        output = rotate_image(output, False)
+    if fn == "Backward":
+        N = backward_energy(output)
+    elif fn == "Forward":
+        N = forward_energy(output)
+
+    content2 = np.sum(N)
+
+    percent_content_loss = ((content - content2) / content) * 100
+
+    return output, percent_content_loss
+
+
+def col_removal(im, num_remove):
+    h, w = im.shape[:2]
+
+    M = backward_energy(im)
+
+    temp = []
+
+    for j, i in enumerate(M):
+        temp.append((sum(i), j))
+
+    temp.sort()
+
+    col_idx = set([temp[i][1] for i in range(num_remove)])
+
+    output = np.zeros_like(im[:h - num_remove], dtype=np.int)
+
+    ind = 0
+    for i, j in enumerate(im):
+        if i in col_idx:
+            continue
+        output[ind] = j
+        ind += 1
 
     return output
 
 
-def carve_main(image,height,width):
+def column_carve(im, dy, dx):
+    im = im.astype(np.float64)
+    h, w = im.shape[:2]
+
+    M = backward_energy(im)
+
+    content = np.sum(M)
+
+    output = im
+
+    if dx < 0:
+        output = rotate_image(output, True)
+        output = col_removal(output, -dx)
+        output = rotate_image(output, False)
+
+    if dy < 0:
+        output = col_removal(output, -dy)
+
+    N = backward_energy(output)
+
+    content2 = np.sum(N)
+
+    percent_content_loss = ((content - content2) / content) * 100
+
+    return output, percent_content_loss
+
+
+def pixels_removal(im, num_remove):
+    h, w, d = im.shape
+
+    M = backward_energy(im)
+
+    output = np.zeros(shape=(h, w - num_remove, d), dtype=np.int)
+
+    for ind, row in enumerate(M):
+        temp = [(i, j) for j, i in enumerate(row)]
+        temp.sort()
+        pixels_idx = set([temp[i][1] for i in range(num_remove)])
+
+        k = 0
+        for i, j in enumerate(im[ind]):
+            if i in pixels_idx:
+                continue
+            output[ind][k] = j
+            k += 1
+
+        # output[ind] = output[ind][:w-num_remove]
+
+    return output
+
+
+def pixels_carve(im, dy, dx):
+    im = im.astype(np.float64)
+    h, w = im.shape[:2]
+
+    M = backward_energy(im)
+
+    content = np.sum(M)
+
+    output = im
+
+    if dx < 0:
+        output = pixels_removal(output, -dx)
+
+    if dy < 0:
+        output = rotate_image(output, True)
+        output = pixels_removal(output, -dy)
+        output = rotate_image(output, False)
+
+    N = backward_energy(output)
+
+    content2 = np.sum(N)
+
+    percent_content_loss = ((content - content2) / content) * 100
+
+    return output, percent_content_loss
+
+
+def Max_2D_sliding_window(M, h_, w_, h, w):
+    rowSum = [[None] * w] * h
+    colSum = [[None] * w] * h
+
+    for i in range(h):
+        for j in range(w):
+            if j == 0:
+                rowSum[i][j] = M[i][j]
+            else:
+                rowSum[i][j] = M[i][j] + rowSum[i][j - 1]
+
+    for i in range(w):
+        for j in range(h):
+            if j == 0:
+                colSum[j][i] = M[j][i]
+            else:
+                colSum[j][i] = M[j][i] + colSum[j - 1][i]
+
+    window = 0
+
+    for i in range(h_ - 1):
+        for j in range(w_ - 1):
+            window += M[i][j]
+
+    maxi = 0
+
+    ans = (None, None)
+
+    start = 0
+
+    for i in range(h_ - 1, h):
+
+        window += rowSum[i][w_ - 1]
+
+        window2 = window
+        start2 = 0
+
+        for j in range(w_ - 1, w):
+
+            if start == 0:
+                window2 += colSum[i][j]
+            else:
+                window2 += colSum[i][j] - colSum[start - 1][j]
+
+            if window2 > maxi:
+                maxi = window2
+                ans = (start, start2)
+
+            if start == 0:
+                window2 -= colSum[i][start2]
+            else:
+                window2 -= colSum[i][start2] - colSum[start - 1][start2]
+
+            start2 += 1
+
+        window -= rowSum[start][w_ - 1]
+        start += 1
+
+    return ans
+
+
+def crop(im, h_, w_, M):
+    h, w, d = im.shape
+
+    M_ = [[M[i][j] for j in range(w)] for i in range(h)]
+
+    x, y = Max_2D_sliding_window(M_, h_, w_, h, w)
+
+    output = np.zeros(shape=(h_, w_, d), dtype=np.int)
+
+    for i in range(h_):
+        for j in range(w_):
+            output[i][j] = im[x + i][y + j]
+
+    return output
+
+
+def croping(im, new_h, new_w):
+    im = im.astype(np.float64)
+    h, w = im.shape[:2]
+
+    M = backward_energy(im)
+
+    content = np.sum(M)
+
+    output = im
+
+    output = crop(output, new_h, new_w, M)
+
+    N = backward_energy(output)
+
+    content2 = np.sum(N)
+
+    percent_content_loss = ((content - content2) / content) * 100
+
+    return output, percent_content_loss
+
+
+
+def carve_main(image,height,width, algo):
 
     IMAGE = image
     OUTPUT = "out.jpeg"
@@ -200,7 +449,7 @@ def carve_main(image,height,width):
         im = imutils.url_to_image(IMAGE)
         h, w = im.shape[:2]
 
-        if SHOULD_DOWNSIZE:
+        if SHOULD_DOWNSIZE and algo in [0,1]:
             if w > DOWNSIZE_WIDTH:
                 im = resize(im, width=DOWNSIZE_WIDTH)
                 h,w = im.shape[:2]
@@ -227,7 +476,22 @@ def carve_main(image,height,width):
             dy = new_h - h
             dx = new_w - w
 
-            output = seam_carve(im, dy, dx, False)
+
+            if algo == 0:
+                output,_ = seam_carve(im, dy, dx, False, fn = 'Backward')
+
+            elif algo == 1:
+                output,_ = seam_carve(im, dy, dx, False, fn = 'Forward')
+
+            elif algo == 2:
+                output,_ = column_carve(im, dy, dx)
+
+            elif algo == 3:
+                output,_ = pixels_carve(im, dy, dx)
+
+            elif algo == 4:
+                output,_ = croping(im, new_h, new_w)
+
 
             # cv2.imwrite(OUTPUT, output)
             # cv2.waitKey(0)
--- carving/views.py
+++ carving/views.py
@@ -12,13 +12,13 @@ from .algo import carve_main
 # Create your views here.
 
 
-def carve(image, height, width):
+def carve(image, height, width, algo):
 
     pk = BytesIO(image)
     old = uploader.upload_resource(pk, folder="SeamCarving")
     pk.flush()
 
-    new = carve_main(old.url, height, width)
+    new = carve_main(old.url, height, width, algo)
 
     pk = BytesIO(new)
     image = uploader.upload_resource(pk, folder="SeamCarving")
@@ -37,7 +37,12 @@ class HomeView(View):
         height = int(request.POST['height'])
         width = int(request.POST['width'])
 
-        old, new = carve(image, height, width)
+        if 'algo' in request.POST:
+            algo = int(request.POST['algo'])
+        else:
+            algo = 0
+
+        old, new = carve(image, height, width, algo)
 
         return render(request, "index.html", {"old": old, "new": new})
 
